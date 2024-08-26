51188c10e1427ce008c035a5169bf7f5dddd9c78
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-08-26 18:18:54 -0500

    Order type implementation

--- product.js
+++ product.js
@@ -33,7 +33,50 @@ class Product {
 
 }
 
+class Order{
+    static countOrder = 0;
+    static get MAX_PRODUCTS(){
+        return 5;
+    }
+
+    constructor(){
+        this._idOrder = ++Order.countOrder;
+        this._products = [];
+        //this.countProductsAdded = 0;
+    }
+
+    get idOrder(){
+        return this._idOrder;
+    }
+
+    addProduct(product){
+        if(this._products.length < Order.MAX_PRODUCTS){
+            this._products.push(product);
+            //this._products[this._countProductsAdded++] = product;
+        }else{
+            console.log('No se pueden agregar mas productos');
+        }
+    }
+
+    calculateTotal(){
+        let total = 0;
+        for(let product of this._products){
+            total += product.price;
+        }
+        return total;
+    }
+
+    showOrder(){
+        let productsOrder = '';
+        for(let product of this._products){
+            productsOrder += product.toString() + ' ';
+        }
+        console.log(`Order: ${this._idOrder}, Total: ${this.calculateTotal()}, Products: ${productsOrder}`);
+    }
+}
+
 let product1 = new Product('Pantalon', 100);
 let product2 = new Product('Camisa 2', 200);
-console.log(product1.toString());
-console.log(product2.toString());
+/* console.log(product1.toString());
+console.log(product2.toString()); */
+
